e878179a05b583fb3d3a35e2d298395c1942b370
vardhanyuvraj <vardhan.yuvraj.singh@gmail.com>
2023-03-31 00:38:51 +0530

    added insert at end and comments for code

--- src/LinkedList/RunnerClass.java
+++ src/LinkedList/RunnerClass.java
@@ -13,6 +13,15 @@ public class RunnerClass {
 		singlyLinkedList.insertAtBegin(646);
 		System.out.println(singlyLinkedList);
 		System.out.println(singlyLinkedList.getListLength());
+		singlyLinkedList.insertAtEnd(8558);
+		System.out.println(singlyLinkedList);
+		System.out.println(singlyLinkedList.getListLength());
+		singlyLinkedList.insertAtBegin(71);
+		System.out.println(singlyLinkedList);
+		System.out.println(singlyLinkedList.getListLength());
+		singlyLinkedList.insertAtEnd(987);
+		System.out.println(singlyLinkedList);
+		System.out.println(singlyLinkedList.getListLength());
 	}
 
 }
--- src/LinkedList/SinglyNode.java
+++ src/LinkedList/SinglyNode.java
@@ -1,25 +1,34 @@
 package LinkedList;
 
+//This class represents a node in a singly linked list
 public class SinglyNode {
 
+	// This variable stores the data associated with the node
 	private int data;
+
+	// This variable stores a reference to the next node in the linked list
 	private SinglyNode next;
 
+	// This is the default constructor for the class
 	public SinglyNode() {
 	}
 
+	// This method returns the data associated with the node
 	public int getData() {
 		return data;
 	}
 
+	// This method sets the data associated with the node
 	public void setData(int data) {
 		this.data = data;
 	}
 
+	// This method returns the next node in the linked list
 	public SinglyNode getNext() {
 		return next;
 	}
 
+	// This method sets the next node in the linked list
 	public void setNext(SinglyNode next) {
 		this.next = next;
 	}
